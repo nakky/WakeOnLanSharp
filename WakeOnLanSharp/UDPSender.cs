954a54b640eea1ba9a33e0b625debdb32b579070
Naoto Nakamura <naoto.nakamura82@gmail.com>
2020-08-06 23:17:42 +0900

    Initial Commit.

---
+++ WakeOnLanSharp/UDPSender.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WakeOnLanSharp
+{
+    public class UDPSender
+    {
+        public const int DefaultBufferSize = 8192;
+
+        int portNum;
+
+        private UdpClient client;
+
+        SemaphoreSlim locker = new SemaphoreSlim(1,1);
+
+        public UDPSender(int portNum, int bufferSize = DefaultBufferSize)
+        {
+            this.portNum = portNum;
+            client = new UdpClient();
+            client.Client.SendBufferSize = bufferSize;
+            client.Client.ReceiveBufferSize = bufferSize;
+        }
+
+        ~UDPSender()
+        {
+            Close();
+        }
+
+        public void Close()
+        {
+            if(client != null){
+                client.Close();
+                client = null;
+            }
+        }
+
+        public async Task Send(string ip, int size, byte[] data)
+        {
+            try
+            {
+                await locker.WaitAsync();
+                await client.SendAsync(data, size, ip, this.portNum);
+            }
+            finally
+            {
+                locker.Release();
+            }
+        }
+
+    }
+}
---
+++ WakeOnLanSharp/WakeOnLan.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace WakeOnLanSharp
+{
+    public class WakeOnLan
+    {
+        public  int PortNum { get; private set; }
+
+        UDPSender sender;
+
+        public WakeOnLan(int portNum)
+        {
+            PortNum = portNum;
+            sender = new UDPSender(PortNum);
+        }
+
+        public void SendMagicPacket(byte[] macAddress, string ip, int times = 3)
+        {
+            byte[] magic = BuildMagicPacket(macAddress);
+            for(int i = 0 ; i < times ; i++)
+            {
+                sender.Send(ip, magic.Length, magic);
+            }
+        }
+
+        public byte[] BuildMagicPacket(byte[] macAddress)
+        {
+            if (macAddress.Length != 6) throw new ArgumentException();
+
+            List<byte> magic = new List<byte>();
+            for (int i = 0; i < 6; i++)
+            {
+                magic.Add(0xff);
+            }
+
+            for (int i = 0; i < 16; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    magic.Add(macAddress[j]);
+                }
+            }
+            return magic.ToArray();
+        }
+
+        public static byte[] ParseMacAddress(string text, char[] separator = null)
+        {
+            if (separator == null) separator = new char[] { ':', '-' };
+            string[] tokens = text.Split(separator);
+
+            byte[] bytes = new byte[6];
+            for (int i = 0; i < 6; i++)
+            {
+                bytes[i] = Convert.ToByte(tokens[i], 16);
+            }
+            return bytes;
+        }
+    }
+}
